944db1c5891ee421ef3c9cf2f03dde4bb2d87089
tuweizhi <tuweizhi@126.com>
2020-06-25 23:30:39 +0800

    uwsgi

--- fish/asgi.py
+++ fish/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for django_to_deploy project.
+ASGI config for fish project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_to_deploy.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fish.settings')
 
 application = get_asgi_application()
--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_to_deploy.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fish.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
